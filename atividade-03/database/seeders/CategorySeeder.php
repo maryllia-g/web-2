95b51020fe1cebe8739ea134160de8c4982549d1
Marília <mariligsc020@gmail.com>
2025-10-22 20:43:15 -0300

    Subindo atividade-03

---
+++ atividade-03/database/seeders/CategorySeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    
+    public function run()
+    {
+        $categories = [
+            'Ficção',
+            'Não-ficção',
+            'Fantasia',
+            'Ciência',
+            'Biografia',
+            'História',
+            'Tecnologia',
+            'Arte',
+            'Culinária',
+            'Viagem'
+        ];
+
+        foreach ($categories as $category) {
+            Category::create(['name' => $category]);
+        }
+    }
+
+}
