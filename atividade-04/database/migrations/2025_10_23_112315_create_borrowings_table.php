2ec688b1479ab19488f5e64d02dd75524396e9da
Marília <mariligsc020@gmail.com>
2025-10-23 09:49:04 -0300

    subindo atividade-04

---
+++ atividade-04/database/migrations/2025_10_23_112315_create_borrowings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    
+    public function up()
+    {
+        Schema::create('borrowings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
+            $table->foreignId('book_id')->constrained()->onDelete('cascade'); 
+            $table->timestamp('borrowed_at')->nullable(); 
+            $table->timestamp('returned_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('borrowings');
+        Schema::enableForeignKeyConstraints();
+    }
+
+};
---
+++ atividade-04/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as FakerFactory;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Cria dados com Faker
+        FakerFactory::create()->unique(true);
+
+        // Cria um usuário de teste
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        // Chama outros seeders
+        $this->call([
+            CategorySeeder::class,
+            AuthorPublisherBookSeeder::class,
+            UserBorrowingSeeder::class,
+        ]);
+    }
+
+    
+}
